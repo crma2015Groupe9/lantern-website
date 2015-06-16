daa5483c3c433a13df5685962fe8626ba0fc61d1
kaymorey <moreirakatia@neuf.fr>
2015-06-16 23:30:29 +0200

    Some fixes for responsive; add favicon

--- sources/javascript/classes/Slider.js
+++ sources/javascript/classes/Slider.js
@@ -11,6 +11,8 @@ var Slider = (function() {
 		this.autoplaySpeed = 5000;
 
 		this.init();
+		this.beforeChange();
+		this.afterChange();
 	}
 
 	Slider.prototype.init = function() {
@@ -37,12 +39,17 @@ var Slider = (function() {
 				{
 					breakpoint: 720,
 					settings: {
-						slidesToShow: 1
+						slidesToShow: 1,
+						centerPadding: 0
 					}
 				}
 			]
 		});
 
+		this.animateLoader();
+	};
+
+	Slider.prototype.beforeChange = function() {
 		this.$.on('beforeChange', function(event, slick, currentSlide, nextSlide) {
 			var $nextSlide = $('.slider [data-slick-index='+nextSlide+']');
 
@@ -58,22 +65,27 @@ var Slider = (function() {
 				$animalName.fadeIn(400);
 			});
 		});
+	};
+
+	Slider.prototype.afterChange = function() {
+		var self = this;
 
 		this.$.on('afterChange', function(event, slick, currentSlide, nextSlide) {
-			var $loader = $('div.loading .loader');
-			TweenLite.to($loader, self.autoplaySpeed / 1000, {width: '100%', ease: Power1.easeInOut, onComplete: function() {
-				TweenLite.to($loader, 0.5, {left: '100%', ease: Power1.easeInOut, onComplete: function() {
-					$loader.css({left: 0, width: 0});
-				}});
-			}});
-			// $('div.loading .loader').animate({
-			// 	width: '100%'
-			// }, self.autoplaySpeed, function() {
-			// 	$('div.loading .loader').animate({
-			// 		width: 0
-			// 	}, 500);
-			// });
+			self.animateLoader();
 		});
+	};
+
+	Slider.prototype.animateLoader = function() {
+		var $loader = $('div.loading .loader');
+		TweenLite.to($loader, this.autoplaySpeed / 1000, {width: '100%', ease: Power1.easeInOut, onComplete: function() {
+			TweenLite.to($loader, 0.5, {left: '100%', ease: Power1.easeInOut, onComplete: function() {
+				$loader.css({left: 0, width: 0});
+			}});
+		}});
+	};
+
+	Slider.prototype.pagination = function() {
+		var self = this;
 
 		$('.pagination button').on('click', function() {
 			var index = $(this).index();
